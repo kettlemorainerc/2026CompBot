f100235b035df369465fcc9b60feac8085291209
kettlemorainerc <kettlemorainerc@gmail.com>
2026-02-19 18:45:32 -0600

    Got rid of a commented piece of code
    There was some leftover code from my testing that was commented out and I just got rid of it.

--- src/main/java/frc/robot/DriveStation.java
+++ src/main/java/frc/robot/DriveStation.java
@@ -82,42 +82,6 @@ public class DriveStation {
 
         bind(hardware);
 
-
-        // RobotConfig config;
-        // try{
-        //     System.out.println("1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
-        //     config = RobotConfig.fromGUISettings();
-
-        //     // Configure AutoBuilder last
-        //     AutoBuilder.configure(
-        //         drivetrain::getPose, // Robot pose supplier
-        //         drivetrain::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
-        //         drivetrain::getCurrentSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-        //         (speeds, feedforwards) -> driveRobotRelative(speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
-        //         new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
-        //                 new PIDConstants(1, 0.0, 0.0), // Translation PID constants
-        //                 new PIDConstants(1, 0.0, 0.0) // Rotation PID constants
-        //         ),
-        //         config, // The robot configuration
-        //         () -> {
-        //         // Boolean supplier that controls when the path will be mirrored for the red alliance
-        //         // This will flip the path being followed to the red side of the field.
-        //         // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
-
-        //         var alliance = DriverStation.getAlliance();
-        //         if (alliance.isPresent()) {
-        //             return alliance.get() == DriverStation.Alliance.Red;
-        //         }
-        //         return false;
-        //         },
-        //         this // Reference to this subsystem to set requirements
-
-        //     );
-        // } catch (Exception e) {
-        //     // Handle exception as needed
-        //     System.out.println(0/0);
-        //     e.printStackTrace();
-        // }
     }
 
     /**
