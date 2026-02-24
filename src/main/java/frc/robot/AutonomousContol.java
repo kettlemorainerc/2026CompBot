74e27fdb257de379fbfd02d4d663e2a53bebac1c
kettlemorainerc <kettlemorainerc@gmail.com>
2026-02-23 18:30:04 -0600

    Added some dubug commands for auto

--- src/main/java/frc/robot/AutonomousContol.java
+++ src/main/java/frc/robot/AutonomousContol.java
@@ -3,8 +3,14 @@ package frc.robot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.command.LauncherControl;
+import frc.robot.command.RPMChangeHolder;
+
+import javax.sound.sampled.SourceDataLine;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 
 public class AutonomousContol {
@@ -12,15 +18,17 @@ public class AutonomousContol {
 private final SendableChooser<Command> autoChooser;
 
     public AutonomousContol(){
-        AutoBuilder autoBuilder = new AutoBuilder();
+        RPMChangeHolder rpm = new RPMChangeHolder(4000);
 
-        
+        NamedCommands.registerCommand("Debug", Commands.print("DEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUG"));
+        NamedCommands.registerCommand("Launcher Forward", new LauncherControl(rpm));
 
         autoChooser = AutoBuilder.buildAutoChooser();
 
         SmartDashboard.putData("Auto Chooser", autoChooser);
 
     }
+
 
     public Command getAuntonomousCommand(){
         return autoChooser.getSelected();
