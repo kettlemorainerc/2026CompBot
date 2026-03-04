63c00406e33b24d6a1c20cd8b26df96a6d885fd5
kettlemorainerc <kettlemorainerc@gmail.com>
2026-03-03 19:59:21 -0600

    Fixed auto to have named commands for all subsystems

--- src/main/java/frc/robot/command/MagicCarpetControls.java
+++ src/main/java/frc/robot/command/MagicCarpetControls.java
@@ -6,31 +6,51 @@ import frc.robot.subsystems.MagicCarpet;
 public class MagicCarpetControls extends NewRepeatedCommand {
     public enum CarpetDirection{
         FLY,
-        FALL
+        FALL,
+        STOP
     }
 
     private final CarpetDirection direction;
     private final MagicCarpet carpet;
+    private final boolean auto;
 
     public MagicCarpetControls(CarpetDirection direction){
         this.direction = direction;
         carpet = RobotHardware.getInstance().carpet;
+        this.auto = false;
+    }
+    public MagicCarpetControls(CarpetDirection direction, Boolean auto){
+        this.direction = direction;
+        carpet = RobotHardware.getInstance().carpet;
+        this.auto = auto;
     }
 
 
     @Override
+    public void initialize(){
+        switch (direction) {
+            case FLY:
+                carpet.carpetFly();
+                break;
+            case FALL:
+                carpet.carpetFall();
+                break;
+            case STOP:
+                carpet.stopCarpet();
+                break;
+        }
+    }
+
+    @Override
     public void execute() {
-        if(direction == CarpetDirection.FLY){
-            carpet.carpetFly();
-        }
-        if(direction == CarpetDirection.FALL){
-            carpet.carpetFall();
-        }
+
     }
 
     @Override
     public void end(boolean interrupted) {
-        carpet.stopCarpet();
+        if(!auto){
+            carpet.stopCarpet();
+        }
     }
     
 }
--- src/main/java/frc/robot/command/RoombaControls.java
+++ src/main/java/frc/robot/command/RoombaControls.java
@@ -8,31 +8,53 @@ import frc.robot.subsystems.Roomba;
 public class RoombaControls extends NewRepeatedCommand {
     public enum RoombaDirection{
         START,
-        REVERSE
+        REVERSE,
+        STOP
     }
 
     private final RoombaDirection direction;
     private final Roomba roomba;
+    private final boolean auto;
 
     public RoombaControls(RoombaDirection direction){
         this.direction = direction;
         roomba = RobotHardware.getInstance().roomba;
+        this.auto = false;
+    }
+
+    public RoombaControls(RoombaDirection direction, Boolean auto){
+        this.direction = direction;
+        roomba = RobotHardware.getInstance().roomba;
+        this.auto = auto;
+    }
+
+    @Override
+    public void initialize(){
+        switch (direction) {
+            case START:
+                roomba.startRoomba();
+                break;
+            case REVERSE:
+                roomba.reverseRoomba();
+                break;
+            case STOP:
+                roomba.stopRoomba();
+                break;
+            default:
+                break;
+        }
     }
 
 
     @Override
     public void execute() {
-        if(direction == RoombaDirection.START){
-            roomba.startRoomba();
-        }
-        if(direction == RoombaDirection.REVERSE){
-            roomba.reverseRoomba();
-        }
     }
 
     @Override
     public void end(boolean interrupted) {
-        roomba.stopRoomba();
+        if(!auto){
+            roomba.stopRoomba();
+        }
     }
     
 }
